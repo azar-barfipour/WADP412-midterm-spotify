9e87908499e6ac5e0dd30126d2aa37be75700787
Azar Barfipour <azar.barfipour@gmail.com>
2022-04-07 14:42:58 -0700

    force to rebuild

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
