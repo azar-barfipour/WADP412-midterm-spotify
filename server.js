fd6b440853c9e1240ce85f77be78a0e0b008b879
Azar Barfipour <azar.barfipour@gmail.com>
2022-04-07 14:50:11 -0700

    last please

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
