481f619ee9e9f9e3b828af01a740657810144307
Azar Barfipour <azar.barfipour@gmail.com>
2022-04-07 14:19:02 -0700

    clear typo

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== "production") çrequire("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
