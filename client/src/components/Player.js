d727d85f391ae2f0d7bb2cc9057b7529c62e253d
Azar Barfipour <azar.barfipour@gmail.com>
2022-04-07 16:13:34 -0700

    change spotify component

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,11 +1,11 @@
-import SpotifyPlayer from "react-spotify-web-playback";
+import SpotifyWebPlayer from "react-spotify-web-playback";
 import classes from "./Player.module.css";
 
 function Player({ accessToken, trackUri }) {
   if (!accessToken) return null;
   return (
     <div className={classes["player-wrapper"]}>
-      <SpotifyPlayer
+      <SpotifyWebPlayer
         token={accessToken}
         showSaveIcon
         uris={trackUri ? [trackUri] : []}
